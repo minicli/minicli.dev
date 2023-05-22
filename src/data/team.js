1b3162401bb9ec756d89ea5b3af3cff979a3c97a
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-05-22 12:50:01 +0100

    Split team section into components

---
+++ src/data/team.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        name: 'Erika Heidi',
+        image: 'https://github.com/erikaheidi.png',
+        role: 'Creator of Minicli',
+        social: [],
+    },
+    {
+        name: 'Steve McDougall',
+        image: 'https://github.com/JustSteveKing.png',
+        role: 'Maintainer at Minicli',
+        social: [],
+    },
+    {
+        name: 'Wendell Adriel',
+        image: 'https://github.com/WendellAdriel.png',
+        role: 'Maintainer at Minicli',
+        social: [],
+    },
+]
