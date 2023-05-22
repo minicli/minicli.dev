e038c0b1fa3391a220fce3ad447523407939ebc4
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-05-22 13:07:17 +0100

    Update the style for the team section

--- src/data/team.js
+++ src/data/team.js
@@ -3,18 +3,36 @@ export default [
         name: 'Erika Heidi',
         image: 'https://github.com/erikaheidi.png',
         role: 'Creator of Minicli',
-        social: [],
+        social: [
+            {
+                name: 'GitHub',
+                icon : '',
+                url: 'https://github.com/erikaheidi'
+            },
+        ],
     },
     {
         name: 'Steve McDougall',
         image: 'https://github.com/JustSteveKing.png',
         role: 'Maintainer at Minicli',
-        social: [],
+        social: [
+            {
+                name: 'GitHub',
+                icon : '',
+                url: 'https://github.com/JustSteveKing'
+            },
+        ],
     },
     {
         name: 'Wendell Adriel',
         image: 'https://github.com/WendellAdriel.png',
         role: 'Maintainer at Minicli',
-        social: [],
+        social: [
+            {
+                name: 'GitHub',
+                icon : '',
+                url: 'https://github.com/WendellAdriel'
+            },
+        ],
     },
 ]
--- src/types/social.ts
+++ src/types/social.ts
@@ -1,4 +1,5 @@
 export default interface Social {
     name: string;
+    icon: string;
     url: string;
 }
